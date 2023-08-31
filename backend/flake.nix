9a7ec9dc003395da3ac41b5b8f2755e44f4c6515
mohankumar.malapureddy <mohankumar.malapureddy@juspay.in>
2023-09-01 02:35:57 +0530

    Aded Lib file and CHANGELOG

--- backend/flake.nix
+++ backend/flake.nix
@@ -22,6 +22,7 @@
       ];
       perSystem = { self', system, lib, config, pkgs, ... }: {
         haskellProjects.default = {
+          projectRoot = ./.;
           imports = [
             inputs.euler-hs.haskellFlakeProjectModules.output
           ];
--- backend/src/Lib.hs
+++ backend/src/Lib.hs
@@ -1,4 +1,4 @@
 module Lib where
 
-someFunc :: IO 
+someFunc :: IO ()
 someFunc = return ()
